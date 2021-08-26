638b447350c0939a2b37bd044491171aa054f101
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-08-26 20:21:41 +0200

    Initial commit

---
+++ Clibeat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Clibeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var VerString = "0.0.1 dev";
+
+            Console.WriteLine("Welcome to Clibeat v"+VerString+"!");
+            Console.WriteLine("Mizu, 2021");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Clibeat
+ Console rhythm game
