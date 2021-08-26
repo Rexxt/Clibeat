4d4e957f23f33894b49756b906a6264dd82d969f
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-08-26 20:39:28 +0200

    Update Program.cs

--- Clibeat/Program.cs
+++ Clibeat/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
+using System.Threading;
 
 namespace Clibeat
 {
     class Program
     {
+
+        static void WriteXY(string text, int x, int y)
+        {
+            var OrigLeft = Console.CursorLeft;
+            var OrigTop = Console.CursorTop;
+            
+            Console.SetCursorPosition(x, y);
+            Console.Write(text);
+            Console.SetCursorPosition(OrigLeft, OrigTop);
+        }
+
         static void Main(string[] args)
         {
             var VerString = "0.0.1 dev";
+            Stopwatch GlobalTime = new Stopwatch();
+            GlobalTime.Start();
 
             Console.WriteLine("Welcome to Clibeat v"+VerString+"!");
             Console.WriteLine("Mizu, 2021");
+            // We'll insert file loading here
+            Thread.Sleep(500);
+            Console.Clear();
+
+            
         }
     }
 }
