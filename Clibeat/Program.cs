4d43e24ed45979bda709132b535d5a05b3a38c93
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-08-27 15:30:07 +0200

    Working time code

--- Clibeat/Program.cs
+++ Clibeat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Clibeat
@@ -18,7 +19,16 @@ namespace Clibeat
 
         static void Main(string[] args)
         {
+            var scene = "MainMenu";
+
             var VerString = "0.0.1 dev";
+            long BPM = 120;
+            double MsPB = 1.0 / BPM * 60000;
+            var GameStart = 0;
+            double GameTick = 0;
+            var Updates = 0;
+            long LastUpdate = 0;
+
             Stopwatch GlobalTime = new Stopwatch();
             GlobalTime.Start();
 
@@ -28,7 +38,21 @@ namespace Clibeat
             Thread.Sleep(500);
             Console.Clear();
 
-            
+            while(true)
+            {
+                var DT = GlobalTime.ElapsedMilliseconds - LastUpdate;
+                LastUpdate = GlobalTime.ElapsedMilliseconds;
+                Updates++;
+                Console.Clear();
+                WriteXY("DT" + DT.ToString(), 0, 0);
+                double SDT = DT / 1000.0;
+                WriteXY("SDT" + SDT.ToString(), 0, 1);
+                WriteXY("UPS" + (1/SDT).ToString(), 0, 2);
+                WriteXY("AUPS" + (Updates/(GlobalTime.ElapsedMilliseconds/1000.0)).ToString(), 0, 3);
+
+                GameTick = (GlobalTime.ElapsedMilliseconds - GameStart) / MsPB;
+                WriteXY("Tick" + GameTick, 0, 5);
+            }
         }
     }
 }
